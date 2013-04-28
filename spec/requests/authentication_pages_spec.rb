8ad91271069b97510a05d6748f8774108e5d7d0f
lhersh1 <lhersh1@umbc.edu>
2013-04-28 21:22:04 +0000

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -56,13 +56,25 @@ end
 
   describe "for non-signed-in users" do
     let(:user) { FactoryGirl.create(:user) }
+     
+    describe "in the Relationships controller" do
+      describe "submitting to the create action" do
+        before { post relationships_path }
+        specify { response.should redirect_to(signin_path) }
+      end
 
+      describe "submitting to the destroy action" do
+        before { delete relationship_path(1) }
+        specify { response.should redirect_to(signin_path) }
+      end
+    end
 
     describe "in the Microposts controller" do
       describe "submitting to the create action" do
        before { post microposts_path }
        specify { response.should redirect_to(signin_path) }
       end
+    
 
      describe "submitting to the destroy action" do
       before { delete micropost_path(FactoryGirl.create(:micropost)) }
@@ -101,6 +113,16 @@ end
      describe "visiting the user index" do
        before { visit users_path }
        it { should have_selector('title', text: 'Sign in') }
+     end
+     
+     describe "visting the following page" do
+       before { visit following_user_path(user) }
+       it { should have_selector('title', text: 'Sign in') }
+     end
+
+     describe "visiting the followers page" do
+      before { visit followers_user_path(user) }
+      it { should have_selector('title', text: 'Sign in') }
      end
   end
    describe "as wrong user" do
